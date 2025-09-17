7e514627be372d542f6f9d5dbfbad98368e37540
Jaqueline Sales <jaquelinesales200729@gmail.com>
2025-09-17 16:43:17 -0300

    commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,10 +2,11 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { MenuBarComponent } from './components/menu-bar/menu-bar.component';
 import { HomeComponent } from './pages/home/home.component';
+import { SonyBarComponent } from './components/sony-bar/sony-bar.component';
   
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, MenuBarComponent, HomeComponent],
+  imports: [RouterOutlet, MenuBarComponent, HomeComponent, SonyBarComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
